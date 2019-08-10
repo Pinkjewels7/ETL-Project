5ab8e12d202e8e2a11a1303a8cb5154e40f68521
Pinkjewels7 <50122088+Pinkjewels7@users.noreply.github.com>
2019-08-10 14:04:51 -0500

    Add files via upload
    Current versions of both

--- Hotel_Reviews.sql
+++ Hotel_Reviews.sql
@@ -20,8 +20,7 @@ SELECT * FROM hotels;
 CREATE TABLE users (
 username VARCHAR,    
 user_city VARCHAR,
-user_state VARCHAR
----CONSTRAINT pk_users PRIMARY KEY (username)	
+user_state VARCHAR	
 );
 
 SELECT * FROM users;
@@ -36,13 +35,3 @@ rating VARCHAR
 );
 
 SELECT * FROM reviews;
-
-ALTER TABLE hotels ADD PRIMARY KEY (hotel_id);
-
-ALTER TABLE users ADD PRIMARY KEY (username);
-
-ALTER TABLE reviews ADD CONSTRAINT fk_hotel_id FOREIGN KEY (hotel_id) 
-REFERENCES hotels (hotel_id);
-
-ALTER TABLE reviews ADD CONSTRAINT fk_username FOREIGN KEY (username) 
-REFERENCES users(username);
