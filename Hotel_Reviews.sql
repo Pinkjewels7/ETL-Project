f603264acabe9c9b78761119f7cbab6e68993e8a
uibeabuchi <50345167+uibeabuchi@users.noreply.github.com>
2019-08-10 14:50:47 -0500

    These are the updated files for the project
    We corrected the issues that plagued us earlier in the project by fixing the index issue and also correcting the parent-child issue.

--- Hotel_Reviews.sql
+++ Hotel_Reviews.sql
@@ -20,7 +20,8 @@ SELECT * FROM hotels;
 CREATE TABLE users (
 username VARCHAR,    
 user_city VARCHAR,
-user_state VARCHAR	
+user_state VARCHAR
+---CONSTRAINT pk_users PRIMARY KEY (username)	
 );
 
 SELECT * FROM users;
@@ -35,3 +36,13 @@ rating VARCHAR
 );
 
 SELECT * FROM reviews;
+
+ALTER TABLE hotels ADD PRIMARY KEY (hotel_id);
+
+ALTER TABLE users ADD PRIMARY KEY (username);
+
+ALTER TABLE reviews ADD CONSTRAINT fk_hotel_id FOREIGN KEY (hotel_id) 
+REFERENCES hotels (hotel_id);
+
+ALTER TABLE reviews ADD CONSTRAINT fk_username FOREIGN KEY (username) 
+REFERENCES users(username);
